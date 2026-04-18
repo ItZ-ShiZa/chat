4ee8e555ae8f5f504ab471d4e083af30be554336
ItZ ShiZa <itz.shiza.zti@gmail.com>
2026-04-18 18:50:50 +0300

    menya eto zaebalo

--- paint.cpp
+++ paint.cpp
@@ -8,6 +8,26 @@ std::string pathToUserChatsList = std::filesystem::current_path().string() + "/c
 std::string pathToUserChatsHistory = std::filesystem::current_path().string() + "/chats/" + name + "/chatsHistory/";
 std::vector<std::string> chatsList;
 
+int stringLength (std::string message) {
+    int length = 0;
+    for (int i = 0; i < static_cast<int>(message.size()); i++) {
+        if ((message[i] & 0xC0) != 0x80) {
+            length++;
+        }
+    }
+    return length;
+}
+
+int substrUTF8 (std::string message, int n) {
+    int length, realLength;
+    for (int i = 0; i < n; i++) {
+        if ((message[i] & 0xC0) != 0x80) {
+            length++;
+        }
+        realLength++;
+    }
+}
+
 void getChatsList () {
     std::string filePath = pathToUserChatsList + "/chatsList.txt";
     std::ifstream file(filePath);
@@ -23,6 +43,59 @@ void getChatsList () {
 void getChatHistory (std::string chatName) {
     std::string filePath = pathToUserChatsHistory + chatName + ".txt";
     
+}
+
+
+void writeFile (std::string fileName, std::string message) {
+    std::string filePath;
+    if (fileName != "chatsList.txt") {
+        filePath = pathToUserChatsList + fileName;
+    } else {
+        filePath = pathToUserChatsList + "/chatsList.txt";
+    }
+    std::ofstream file(filePath, std::ios::app);
+    file << message << std::endl;
+}
+
+void updateFile (std::string fileName, std::string message) {
+    std::string tempMessage;
+    int n = 0, pos = -1, start = 0;
+
+    while (message.size() > 0) {
+        tempMessage = message.substr(0, 96);
+        pos = tempMessage.find_last_of(' ');
+        if (pos != -1) {
+            writeFile(fileName, tempMessage);
+        } else {
+            tempMessage = tempMessage.substr(0, pos);
+        }
+    }
+
+
+
+
+    // for (int i = 0; i < stringLength(message); i++) {
+    //     if (n < 96) {
+    //         if (message[i] != ' ') {
+    //             tempWord += message[i];
+    //         } else {
+    //             pos = i;
+    //             tempMessage += tempWord;
+    //             tempWord = "";
+    //         }
+    //     } else {
+    //         writeFile(fileName, tempMessage);
+    //         tempMessage = "";
+    //         if (pos != -1) {
+    //             n = stringLength(tempWord);
+    //             start = i - n;
+    //         } else {
+    //             tempWord = "";
+    //             n = 0;
+    //             start = i;
+    //         }
+    //     }
+    // }
 }
 
 
@@ -49,7 +122,6 @@ void print_a (const char* str, int lines, int countInLine, int spaces, int x = -
         }
     }
 }
-
 void printChat () {
     print_a("┌", 1, 1, 0, 0, 0);
     print_a("─", 1, 98, 0);
@@ -59,7 +131,6 @@ void printChat () {
     print_a("─", 1, 98, 0);
     print_a("┘", 1, 1, 0);
 }
-
 void printChatList () {
     print_a("┌", 1, 1, 0, 101, 0);
     print_a("─", 1, 38, 0);
@@ -81,5 +152,8 @@ int main() {
     std::cout << "\u001b[2J\u001b[H" << std::flush;
     printChat();
     printChatList();
+
+    updateFile("chatsList.txt", "M2");
+
     std::cout << std::endl;
 }
